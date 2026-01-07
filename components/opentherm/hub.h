1fee30916ff0f9c0ce726eadcff06f25eda12f28
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-07 15:23:27 +0100

    Update hub.h

--- components/opentherm/hub.h
+++ components/opentherm/hub.h
@@ -5,25 +5,20 @@
 #include "esphome/core/defines.h"
 #include "esphome/core/hal.h"
 #include "esphome/core/log.h"
-
 #include "opentherm.h"
 
 #ifdef OPENTHERM_USE_SENSOR
 #include "esphome/components/sensor/sensor.h"
 #endif
-
 #ifdef OPENTHERM_USE_BINARY_SENSOR
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #endif
-
 #ifdef OPENTHERM_USE_SWITCH
 #include "esphome/components/opentherm/switch/opentherm_switch.h"
 #endif
-
 #ifdef OPENTHERM_USE_OUTPUT
 #include "esphome/components/opentherm/output/opentherm_output.h"
 #endif
-
 #ifdef OPENTHERM_USE_NUMBER
 #include "esphome/components/opentherm/number/opentherm_number.h"
 #endif
@@ -32,7 +27,6 @@
 #include <memory>
 #include <unordered_map>
 #include <unordered_set>
-
 #include "opentherm_macros.h"
 
 namespace esphome {
@@ -41,22 +35,101 @@ namespace opentherm {
 static const uint8_t REPEATING_MESSAGE_ORDER = 255;
 static const uint8_t INITIAL_UNORDERED_MESSAGE_ORDER = 254;
 
-// OpenTherm component for ESPHome
 class OpenthermHub : public Component {
  protected:
-  // Communication pins for the OpenTherm interface
   InternalGPIOPin *in_pin_, *out_pin_;
-  // The OpenTherm interface
   std::unique_ptr<OpenTherm> opentherm_;
 
   OPENTHERM_SENSOR_LIST(OPENTHERM_DECLARE_SENSOR, )
-
   OPENTHERM_BINARY_SENSOR_LIST(OPENTHERM_DECLARE_BINARY_SENSOR, )
-
   OPENTHERM_SWITCH_LIST(OPENTHERM_DECLARE_SWITCH, )
-
   OPENTHERM_NUMBER_LIST(OPENTHERM_DECLARE_NUMBER, )
-
   OPENTHERM_OUTPUT_LIST(OPENTHERM_DECLARE_OUTPUT, )
+  OPENTHERM_INPUT_SENSOR_LIST(OPENTHERM_DECLARE_INPUT_SENSOR, )
+  OPENTHERM_SETTING_LIST(OPENTHERM_DECLARE_SETTING, )
 
-  OPENTHERM_INPUT_SENSOR_LIST(OPENTHER
+  bool sending_initial_ = true;
+  std::unordered_map<MessageId, uint8_t> configured_messages_;
+  std::vector<MessageId> messages_;
+  std::vector<MessageId>::const_iterator message_iterator_;
+  uint32_t last_conversation_start_ = 0;
+  uint32_t last_conversation_end_ = 0;
+  OperationMode last_mode_ = IDLE;
+  OpenthermData last_request_;
+  bool sync_mode_ = false;
+
+  CallbackManager<void(OpenthermData &)> before_send_callback_;
+  CallbackManager<void(OpenthermData &)> before_process_response_callback_;
+
+  OpenthermData build_request_(MessageId request_id) const;
+  bool handle_error_(OperationMode mode);
+  void handle_protocol_error_();
+  void handle_timeout_error_();
+  void handle_timer_error_();
+  void stop_opentherm_();
+  void start_conversation_();
+  void read_response_();
+  void check_timings_(uint32_t cur_time);
+  bool should_skip_loop_(uint32_t cur_time) const;
+  void sync_loop_();
+  void write_initial_messages_(std::vector<MessageId> &target);
+  void write_repeating_messages_(std::vector<MessageId> &target);
+
+  template<typename F> bool spin_wait_(uint32_t timeout, F func) {
+    auto start_time = millis();
+    while (func()) {
+      yield();
+      if (millis() - start_time >= timeout) return false;
+    }
+    return true;
+  }
+
+ public:
+  OpenthermHub();
+  void process_response(OpenthermData &data);
+  void set_in_pin(InternalGPIOPin *in_pin) { this->in_pin_ = in_pin; }
+  void set_out_pin(InternalGPIOPin *out_pin) { this->out_pin_ = out_pin; }
+
+  OPENTHERM_SENSOR_LIST(OPENTHERM_SET_SENSOR, )
+  OPENTHERM_BINARY_SENSOR_LIST(OPENTHERM_SET_BINARY_SENSOR, )
+  OPENTHERM_SWITCH_LIST(OPENTHERM_SET_SWITCH, )
+  OPENTHERM_NUMBER_LIST(OPENTHERM_SET_NUMBER, )
+  OPENTHERM_OUTPUT_LIST(OPENTHERM_SET_OUTPUT, )
+  OPENTHERM_INPUT_SENSOR_LIST(OPENTHERM_SET_INPUT_SENSOR, )
+  OPENTHERM_SETTING_LIST(OPENTHERM_SET_SETTING, )
+
+  void send_raw_message(OpenthermData &data) {
+    if (this->opentherm_ != nullptr) {
+      this->opentherm_->send(data);
+      this->last_conversation_start_ = millis();
+      this->last_mode_ = WRITE;
+      ESP_LOGD("opentherm_hub", "Wysłano ID: %d", data.id);
+    }
+  }
+
+  void add_initial_message(MessageId message_id) { this->configured_messages_[message_id] = INITIAL_UNORDERED_MESSAGE_ORDER; }
+  void add_initial_message(MessageId message_id, uint8_t order) { this->configured_messages_[message_id] = order; }
+  void add_repeating_message(MessageId message_id) { this->configured_messages_[message_id] = REPEATING_MESSAGE_ORDER; }
+
+  bool ch_enable = true, dhw_enable = true, cooling_enable = false, otc_active = false, ch2_active = false, summer_mode_active = false, dhw_block = false;
+  void set_ch_enable(bool value) { this->ch_enable = value; }
+  void set_dhw_enable(bool value) { this->dhw_enable = value; }
+  void set_cooling_enable(bool value) { this->cooling_enable = value; }
+  void set_otc_active(bool value) { this->otc_active = value; }
+  void set_ch2_active(bool value) { this->ch2_active = value; }
+  void set_summer_mode_active(bool value) { this->summer_mode_active = value; }
+  void set_dhw_block(bool value) { this->dhw_block = value; }
+  void set_sync_mode(bool sync_mode) { this->sync_mode_ = sync_mode; }
+
+  void add_on_before_send_callback(std::function<void(OpenthermData &)> &&callback) { this->before_send_callback_.add(std::move(callback)); }
+  void add_on_before_process_response_callback(std::function<void(OpenthermData &)> &&callback) { this->before_process_response_callback_.add(std::move(callback)); }
+
+  float get_setup_priority() const override { return setup_priority::HARDWARE; }
+  void setup() override;
+  void on_shutdown() override;
+  void loop() override;
+  void dump_config() override;
+};
+
+}  // namespace opentherm
+}  // namespace esphome
