6240697e0418f13aa225b2eb86814476cde0a195
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-07 15:59:41 +0100

    Update const.py

--- components/opentherm/const.py
+++ components/opentherm/const.py
@@ -3,6 +3,9 @@ OPENTHERM = "opentherm"
 CONF_OPENTHERM_ID = "opentherm_id"
 CONF_DATA_TYPE = "data_type"
 
+# Nowa stała dla Twojego sensora przepływu
+CONF_FLOW_RATE = "flow_rate"
+
 SENSOR = "sensor"
 BINARY_SENSOR = "binary_sensor"
 SWITCH = "switch"
