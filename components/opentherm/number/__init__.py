efed2717930943bd675f17cd75ce62cd72987389
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-07 15:07:52 +0100

    Update __init__.py

--- components/opentherm/number/__init__.py
+++ components/opentherm/number/__init__.py
@@ -1,66 +1,54 @@
-from typing import Any
-
 import esphome.codegen as cg
-from esphome.components import number
 import esphome.config_validation as cv
-from esphome.const import CONF_INITIAL_VALUE, CONF_RESTORE_VALUE, CONF_STEP
+from esphome.components import number
+from esphome.const import (
+    CONF_ID,
+    CONF_NAME,
+    CONF_INITIAL_VALUE,
+    CONF_RESTORE_VALUE,
+    CONF_MIN_VALUE,
+    CONF_MAX_VALUE,
+    CONF_STEP,
+)
+# Importujemy namespace i komponent główny z folderu wyżej
+from .. import opentherm_ns, OpenthermComponent, CONF_OPENTHERM_ID
 
-from .. import const, generate, input, schema, validate
-
-DEPENDENCIES = [const.OPENTHERM]
-COMPONENT_TYPE = const.NUMBER
-
-OpenthermNumber = generate.opentherm_ns.class_(
-    "OpenthermNumber", number.Number, cg.Component, input.OpenthermInput
+# Deklaracja klasy C++
+OpenthermNumber = opentherm_ns.class_(
+    "OpenthermNumber", number.Number, cg.Component
 )
 
+CONF_MSG_ID = "msg_id"
 
-async def new_openthermnumber(config: dict[str, Any]) -> cg.Pvariable:
-    var = await number.new_number(
-        config,
-        min_value=config[input.CONF_min_value],
-        max_value=config[input.CONF_max_value],
-        step=config[input.CONF_step],
-    )
+CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(OpenthermNumber),
+        cv.GenerateID(CONF_OPENTHERM_ID): cv.use_id(OpenthermComponent),
+        cv.Required(CONF_NAME): cv.string,
+        cv.Required(CONF_MSG_ID): cv.uint8_t, # To odblokowuje msg_id w YAML
+        cv.Optional(CONF_INITIAL_VALUE): cv.float_,
+        cv.Optional(CONF_RESTORE_VALUE, default=False): cv.bool_,
+        cv.Optional(CONF_MIN_VALUE, default=0): cv.float_,
+        cv.Optional(CONF_MAX_VALUE, default=100): cv.float_,
+        cv.Optional(CONF_STEP, default=1): cv.float_,
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_pointer(config[CONF_ID])
     await cg.register_component(var, config)
-    input.generate_setters(var, config)
-
-    if (initial_value := config.get(CONF_INITIAL_VALUE)) is not None:
-        cg.add(var.set_initial_value(initial_value))
-    if (restore_value := config.get(CONF_RESTORE_VALUE)) is not None:
-        cg.add(var.set_restore_value(restore_value))
-
-    return var
-
-
-def get_entity_validation_schema(entity: schema.InputSchema) -> cv.Schema:
-    return (
-        number.number_schema(
-            OpenthermNumber, unit_of_measurement=entity.unit_of_measurement
-        )
-        .extend(
-            {
-                cv.Optional(CONF_STEP, entity.step): cv.float_,
-                cv.Optional(CONF_INITIAL_VALUE): cv.float_,
-                cv.Optional(CONF_RESTORE_VALUE): cv.boolean,
-            }
-        )
-        .extend(input.input_schema(entity))
-        .extend(cv.COMPONENT_SCHEMA)
-    )
-
-
-CONFIG_SCHEMA = validate.create_component_schema(
-    schema.INPUTS, get_entity_validation_schema
-)
-
-
-async def to_code(config: dict[str, Any]) -> None:
-    keys = await generate.component_to_code(
-        COMPONENT_TYPE,
-        schema.INPUTS,
-        OpenthermNumber,
-        generate.create_only_conf(new_openthermnumber),
+    await number.register_number(
+        var,
         config,
+        min_value=config[CONF_MIN_VALUE],
+        max_value=config[CONF_MAX_VALUE],
+        step=config[CONF_STEP],
     )
-    generate.define_readers(COMPONENT_TYPE, keys)
+
+    parent = await cg.get_variable(config[CONF_OPENTHERM_ID])
+    cg.add(var.set_parent(parent))
+    cg.add(var.set_message_id(config[CONF_MSG_ID]))
+
+    if CONF_INITIAL_VALUE in config:
+        cg.add(var.set_initial_value(config[CONF_INITIAL_VALUE]))
+    cg.add(var.set_restore_value(config[CONF_RESTORE_VALUE]))
