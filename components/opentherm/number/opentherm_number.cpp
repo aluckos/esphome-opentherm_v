55b1e7f998cf54f215b37d76c8997ef85571449b
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-07 15:25:16 +0100

    Update opentherm_number.cpp

--- components/opentherm/number/opentherm_number.cpp
+++ components/opentherm/number/opentherm_number.cpp
@@ -1,4 +1,5 @@
 #include "opentherm_number.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace opentherm {
@@ -6,56 +7,23 @@ namespace opentherm {
 static const char *const TAG = "opentherm.number";
 
 void OpenthermNumber::control(float value) {
-  this->publish_state(value);
-
-  // Zapisywanie stanu, jeśli włączone
-  if (this->restore_value_)
-    this->pref_.save(&value);
-
-  // Logika wysyłania komendy OpenTherm
-  if (this->is_any_number_ && this->parent_ != nullptr) {
-    // Brink zwykle przyjmuje wartości jako u16 (np. 0-100 dla wentylacji)
-    uint16_t data = (uint16_t)value;
+  if (this->parent_ != nullptr && this->message_id_ != 0) {
+    OpenthermData data;
+    data.type = MessageType::WRITE_DATA;
+    data.id = this->message_id_;
+    data.u16((uint16_t)value); 
     
-    // Wrzucamy zapytanie do kolejki huba
-    this->parent_->enqueue_request(OpenThermMessage(
-        OpenThermMessageType::WRITE_DATA,
-        (OpenThermMessageID)this->message_id_,
-        data
-    ));
+    // Używamy nowej metody, którą dodaliśmy do hub.h
+    this->parent_->send_raw_message(data);
+    this->publish_state(value);
     
-    ESP_LOGD(TAG, "Wysłano WRITE_DATA ID %d, Wartość: %u", this->message_id_, data);
-  } else {
-    ESP_LOGW(TAG, "Próba zmiany wartości, ale nie ustawiono msg_id lub brak połączenia z hubem!");
+    ESP_LOGD(TAG, "Wysłano nastawę OpenTherm ID %d: %.1f", this->message_id_, value);
   }
-}
-
-void OpenthermNumber::setup() {
-  float value;
-  if (!this->restore_value_) {
-    value = this->initial_value_;
-  } else {
-    this->pref_ = global_preferences->make_preference<float>(this->get_preference_hash());
-    if (!this->pref_.load(&value)) {
-      if (!std::isnan(this->initial_value_)) {
-        value = this->initial_value_;
-      } else {
-        value = this->traits.get_min_value();
-      }
-    }
-  }
-  this->publish_state(value);
 }
 
 void OpenthermNumber::dump_config() {
-  LOG_NUMBER("", "OpenTherm Number", this);
-  ESP_LOGCONFIG(TAG,
-                "  Restore value: %s", YESNO(this->restore_value_));
-  ESP_LOGCONFIG(TAG,
-                "  Initial value: %.2f", this->initial_value_);
-  if (this->is_any_number_) {
-    ESP_LOGCONFIG(TAG, "  Message ID: %d (Any Number Mode)", this->message_id_);
-  }
+  LOG_NUMBER(TAG, "OpenTherm Number", this);
+  ESP_LOGCONFIG(TAG, "  Message ID: %u", this->message_id_);
 }
 
 }  // namespace opentherm
