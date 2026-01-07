d40dccf9373f416703877ec7e7e421071980d4ae
aluckos <59487836+aluckos@users.noreply.github.com>
2026-01-07 15:04:55 +0100

    Update opentherm_number.cpp

--- components/opentherm/number/opentherm_number.cpp
+++ components/opentherm/number/opentherm_number.cpp
@@ -8,8 +8,26 @@ static const char *const TAG = "opentherm.number";
 void OpenthermNumber::control(float value) {
   this->publish_state(value);
 
+  // Zapisywanie stanu, jeśli włączone
   if (this->restore_value_)
     this->pref_.save(&value);
+
+  // Logika wysyłania komendy OpenTherm
+  if (this->is_any_number_ && this->parent_ != nullptr) {
+    // Brink zwykle przyjmuje wartości jako u16 (np. 0-100 dla wentylacji)
+    uint16_t data = (uint16_t)value;
+    
+    // Wrzucamy zapytanie do kolejki huba
+    this->parent_->enqueue_request(OpenThermMessage(
+        OpenThermMessageType::WRITE_DATA,
+        (OpenThermMessageID)this->message_id_,
+        data
+    ));
+    
+    ESP_LOGD(TAG, "Wysłano WRITE_DATA ID %d, Wartość: %u", this->message_id_, data);
+  } else {
+    ESP_LOGW(TAG, "Próba zmiany wartości, ale nie ustawiono msg_id lub brak połączenia z hubem!");
+  }
 }
 
 void OpenthermNumber::setup() {
@@ -32,10 +50,12 @@ void OpenthermNumber::setup() {
 void OpenthermNumber::dump_config() {
   LOG_NUMBER("", "OpenTherm Number", this);
   ESP_LOGCONFIG(TAG,
-                "  Restore value: %d\n"
-                "  Initial value: %.2f\n"
-                "  Current value: %.2f",
-                this->restore_value_, this->initial_value_, this->state);
+                "  Restore value: %s", YESNO(this->restore_value_));
+  ESP_LOGCONFIG(TAG,
+                "  Initial value: %.2f", this->initial_value_);
+  if (this->is_any_number_) {
+    ESP_LOGCONFIG(TAG, "  Message ID: %d (Any Number Mode)", this->message_id_);
+  }
 }
 
 }  // namespace opentherm
